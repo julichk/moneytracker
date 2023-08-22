1ebb1589de324f824f672cbd6327dec82cc7b0dd
“julichk” <julia01.smith01@gmail.com>
2023-08-22 17:41:37 +0300

    hooks

---
+++ src/components/Balance/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div `
+padding: 5px;
+text-align: center;
+`
+Wrapper.displayNmae = 'Wrapper';
---
+++ src/components/Counter/index.stories.js
@@ -0,0 +1,11 @@
+import Counter from '.';
+
+export default {
+  title: 'Example/Counter',
+  component: Counter,
+};
+
+const Template = (args) => <Counter {...args} />;
+
+export const Income = Template.bind({});
+Income.args = {};
